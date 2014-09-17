08fe9776fc4baa2f83d218c0e3aac360bc5e1387
Takeshi KOMIYA <i.tkomiya@gmail.com>
2014-09-17 19:07:02 +0900

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.1:
+
+* Update ISO image to texlive2014-20140525.iso
+
 ## 0.2.0:
 
 * Add configuration: node['texlive']['timeout']
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "i.tkomiya@gmail.com"
 license          "Apache 2.0"
 description      "Installs TeXLive"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 
 %w{ fedora redhat centos ubuntu debian amazon }.each do |os|
   supports os
