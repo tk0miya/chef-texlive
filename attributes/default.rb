651c68ab7764cb5c0cde712cb825ab4bec29902b
Takeshi KOMIYA <i.tkomiya@gmail.com>
2014-09-17 19:04:37 +0900

    Fix #3 update URL of ISO image

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default['texlive']['dvd_url'] = "http://ftp.jaist.ac.jp/pub/CTAN/systems/texlive/Images/texlive2012-20120701.iso"
+default['texlive']['dvd_url'] = "http://ftp.jaist.ac.jp/pub/CTAN/systems/texlive/Images/texlive2014-20140525.iso"
 default['texlive']['timeout'] = 1800  # 30min
